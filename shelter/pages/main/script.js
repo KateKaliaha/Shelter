23888db02e27dc33e7cd9328f53897f188b6e656
Katherina Kaliaha <mellinda077@mail.ru>
2022-04-17 17:55:47 +0300

    feat: make adaptive 320px for main page

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,5 +1,8 @@
 const itemLink = document.querySelectorAll('.nav-link');
-const navList = document.querySelector('.nav-list')
+const navList = document.querySelector('.nav-list');
+const btnBurger = document.querySelector('.burger');
+const spanBurger = document.querySelectorAll('.line');
+const headerNav = document.querySelector('.header-nav')
 
 
 function changeStatusLink (event) {
@@ -10,3 +13,30 @@ function changeStatusLink (event) {
 }
 
 navList.addEventListener('click', changeStatusLink)
+
+// console.log(btnBurger)
+
+// function changeMenuBurger (event) {
+//     if (event.target.classList.contains('link')) {
+//     event.target.classList.toggle('active');
+//     console.log(spanBurger)
+//     }
+    
+// }
+
+
+btnBurger.addEventListener('click', function(){
+	btnBurger.classList.toggle('active-btn');
+    headerNav.classList.toggle('active-nav')
+	// menu.classList.toggle('active');
+})
+
+(function (){
+    if(window.innerWidth<=768) {
+        for (let i = 0; i < navList.length; i += 1)
+        navList[i].addEventListener('click', () => {
+            navList.classList.remove('active-nav')
+        });
+    }
+} ())
+
